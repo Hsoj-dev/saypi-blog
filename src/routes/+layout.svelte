c7ebc88ab8573412cf6afba2bc0e4e31d48a5a54
Jhollow <jhollowguy@gmail.com>
2026-04-06 21:52:58 +0800

    chore(setup): setup vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,10 @@
 	
 	import { dev } from '$app/environment';
     import { injectAnalytics } from '@vercel/analytics/sveltekit';
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
   
     injectAnalytics({ mode: dev ? 'development' : 'production' });
+    injectSpeedInsights();
 
 	let { children } = $props();
 </script>
