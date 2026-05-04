51b68bbc8bdc2b9769ce95d577b618624464144f
Jhollow <jhollowguy@gmail.com>
2026-05-04 20:01:38 +0800

    fix(logging): enable sentry logging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
 <script lang='ts'>
 	import Main from "$lib/components/layout/public/Main.svelte";
 
+	import * as Sentry from "@sentry/sveltekit";
+
+	Sentry.logger.info("Unlocking the log dashboard!");
+
 	let { data } = $props();
 </script>
 
