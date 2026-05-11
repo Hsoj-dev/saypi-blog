57c70b03877bca573d6994d0d32e2cb0dcba6b3c
Jhollow <jhollowguy@gmail.com>
2026-05-11 21:24:54 +0800

    fix(logging): change console.log to only be in dev mode

--- src/lib/helpers/logger.ts
+++ src/lib/helpers/logger.ts
@@ -1,12 +1,15 @@
+import { dev } from '$app/environment';
 import * as Sentry from "@sentry/node"; 
 
 export function logInfo(event: string, data: Record<string, unknown> = {}) {
-  console.log({
-    level: "INFO",
-    event,
-    timestamp: new Date().toISOString(),
-    ...data
-  });
+  if (dev) {
+    console.log({
+      level: "INFO",
+      event,
+      timestamp: new Date().toISOString(),
+      ...data
+    });
+  }
 
   Sentry.logger.info(Sentry.logger.fmt`[INFO] ${event}`, {
     timestamp: new Date().toISOString(),
@@ -15,12 +18,14 @@ export function logInfo(event: string, data: Record<string, unknown> = {}) {
 }
 
 export function logWarn(event: string, data: Record<string, unknown> = {}) {
-  console.warn({
-    level: "WARN",
-    event,
-    timestamp: new Date().toISOString(),
-    ...data
-  });
+  if (dev) {
+    console.warn({
+      level: "WARN",
+      event,
+      timestamp: new Date().toISOString(),
+      ...data
+    });
+  }
 
   Sentry.logger.warn(Sentry.logger.fmt`[WARN] ${event}`, {
     timestamp: new Date().toISOString(),
@@ -29,12 +34,14 @@ export function logWarn(event: string, data: Record<string, unknown> = {}) {
 }
 
 export function logError(event: string, data: Record<string, unknown> = {}) {
-  console.error({
-    level: "ERROR",
-    event,
-    timestamp: new Date().toISOString(),
-    ...data
-  });
+  if (dev) {
+    console.error({
+      level: "ERROR",
+      event,
+      timestamp: new Date().toISOString(),
+      ...data
+    });
+  }
 
   Sentry.logger.error(Sentry.logger.fmt`[ERROR] ${event}`, {
     timestamp: new Date().toISOString(),
